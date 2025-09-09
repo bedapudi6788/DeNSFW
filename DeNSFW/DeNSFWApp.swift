b62de96d829278ba466b4cd6e43ebf48e3a5337a
Praneeth Bedapudi <praneeth@bpraneeth.com>
2025-09-09 16:10:37 +0530

    Initial Commit

---
+++ DeNSFW/DeNSFWApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeNSFWApp.swift
+//  DeNSFW
+//
+//  Created by Bedapudi Praneeth on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct DeNSFWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
